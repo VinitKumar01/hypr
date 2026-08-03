52b34e141bb4b29df16c0c75b8fabed0718de4ff
vinit <vinitk81144@gmail.com>
2026-08-03 22:31:36 +0530

    Fixed the key press delay

--- input.lua
+++ input.lua
@@ -7,8 +7,6 @@ hl.config({
 		kb_layout = "us",
 		follow_mouse = 1,
 		sensitivity = 0,
-		repeat_rate = 40,
-		repeat_delay = 250,
 		numlock_by_default = true,
 		touchpad = {
 			natural_scroll = true,
